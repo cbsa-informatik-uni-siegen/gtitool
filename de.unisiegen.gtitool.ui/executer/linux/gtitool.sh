aec1a4b743ae9eebf54a5b463ff75e63da4fa0d7
Christian Fehler <christian.fehler@gmx.de>
2007-10-18 15:04:09 +0000

    - Add a execute file for linux - Add a dummy execute file for windows - Extend the build.xml file
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/gtitool/trunk@29 d1fb0f2a-2248-4f96-a540-7c12a6a72a62

---
+++ de.unisiegen.gtitool.ui/executer/linux/gtitool.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+#
+# $Id$
+#
+# Startscript for the GTITool application on Linux/Unix.
+#
+
+# Check if a JRE is available
+if ! type java >/dev/null 2>&1; then
+	cat >&2 <<EOF
+No suitable JRE found. Please download and install a JRE and make sure the
+java binary is in the PATH.
+EOF
+	exit 1
+fi
+
+# determine the real path of the shell script (somewhat hacky)
+if echo "$0" | grep "^/" > /dev/null; then
+	REALPATH=`dirname $0`;
+else
+	REALPATH=`pwd`
+fi
+
+# determine the $XDG_DATA_HOME directory
+test x"$XDG_DATA_HOME" != x"" || export XDG_DATA_HOME="$HOME/.local/share"
+
+# install the mime types
+mkdir -p "$XDG_DATA_HOME/mime/packages"
+cat > "$XDG_DATA_HOME/mime/packages/gtitool.xml" <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
+  <mime-type type="text/x-gtitool">
+    <sub-class-of type="text/plain" />
+    <comment>GTITool source code</comment>
+    <comment xml:lang="de">GTITool Quelltext</comment>
+    <glob pattern="*.[Dd][Ee][Aa]" />
+  </mime-type>
+</mime-info>
+EOF
+update-mime-database "$XDG_DATA_HOME/mime" > /dev/null
+
+# install the .desktop file
+mkdir -p "$XDG_DATA_HOME/applications"
+cat > "$XDG_DATA_HOME/applications/gtitool.desktop" <<EOF
+[Desktop Entry]
+Encoding=UTF-8
+Version=1.0
+Type=Application
+NoDisplay=false
+Name=GTITool @de.unisiegen.gtitool.ui.version@
+Comment=GTI education tool
+Comment[de]=GTI Lernwerkzeug
+TryExec="$REALPATH/gtitool.sh"
+Exec="$REALPATH/gtitool.sh" %F
+MimeType=text/x-gtitool
+StartupNotify=false
+Categories=Education;Development;Java;
+EOF
+update-desktop-database "$XDG_DATA_HOME/applications" > /dev/null
+
+# execute the application
+exec java -jar "$REALPATH/de.unisiegen.gtitool.ui-@de.unisiegen.gtitool.ui.version@.jar" "$@"

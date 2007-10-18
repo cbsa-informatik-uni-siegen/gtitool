efab640936c25c7a25752b3dca25ae5a0fc9d49e
Christian Fehler <christian.fehler@gmx.de>
2007-10-18 11:07:45 +0000

    Do not use the GTK look and feel because of different problems.
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/gtitool/trunk@23 d1fb0f2a-2248-4f96-a540-7c12a6a72a62

--- de.unisiegen.gtitool.ui/source/de/unisiegen/gtitool/ui/Start.java
+++ de.unisiegen.gtitool.ui/source/de/unisiegen/gtitool/ui/Start.java
@@ -1,8 +1,8 @@
-package de.unisiegen.gtitool.ui ;
+package de.unisiegen.gtitool.ui;
 
 
-import java.awt.EventQueue ;
-import javax.swing.UIManager ;
+import java.awt.EventQueue;
+import javax.swing.UIManager;
 
 
 /**
@@ -12,28 +12,43 @@ import javax.swing.UIManager ;
  */
 public class Start
 {
+
   /**
    * The main entry point for the GTITool project. This method also sets up
    * native look and feel for the platform if possible.
    * 
    * @param pArguments The command line arguments.
    */
-  public static void main ( String [ ] pArguments )
+  public static void main ( String [] pArguments )
   {
     try
     {
-      UIManager.setLookAndFeel ( UIManager.getSystemLookAndFeelClassName ( ) ) ;
+      /*
+       * Try to setup native look and feel for the platform, if the native look
+       * and feel is not GTKLookAndFeel, because of different problems with this
+       * look abd feel.
+       */
+      String nativeLAF = UIManager.getSystemLookAndFeelClassName ();
+      if ( nativeLAF.contains ( "GTK" ) ) //$NON-NLS-1$
+      {
+        UIManager.setLookAndFeel ( "javax.swing.plaf.metal.MetalLookAndFeel" ); //$NON-NLS-1$
+      }
+      else
+      {
+        UIManager.setLookAndFeel ( UIManager.getSystemLookAndFeelClassName () );
+      }
     }
     catch ( Exception e )
     {
       // Do nothing
     }
-    EventQueue.invokeLater ( new Runnable ( )
+    EventQueue.invokeLater ( new Runnable ()
     {
-      public void run ( )
+
+      public void run ()
       {
-        System.out.println ( "Implement this ..." ) ; //$NON-NLS-1$
+        System.out.println ( "Implement this ..." ); //$NON-NLS-1$
       }
-    } ) ;
+    } );
   }
 }

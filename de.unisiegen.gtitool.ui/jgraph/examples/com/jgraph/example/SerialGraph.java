3d93ed87a2df69eb97e1758fd301c4dde6b0df12
Christian Fehler <christian.fehler@gmx.de>
2007-10-25 14:48:56 +0000

    Add the source of the JGraph and attach it.
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/gtitool/trunk@71 d1fb0f2a-2248-4f96-a540-7c12a6a72a62

---
+++ de.unisiegen.gtitool.ui/jgraph/examples/com/jgraph/example/SerialGraph.java
@@ -0,0 +1,78 @@
+/*
+ * @(#)EditorGraph.java 3.3 23-APR-04
+ *  
+ * Copyright (c) 2001-2005, Gaudenz Alder
+ * 
+ * See LICENSE file in distribution for licensing details of this source file
+ */
+package com.jgraph.example;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import javax.swing.JFrame;
+
+import org.jgraph.JGraph;
+
+/**
+ * An example to demonstrate the use of serialization in JGraph. As of version
+ * 5.7.5, the JGraph object is fully serializable. In a real-world situation,
+ * the graph must be prepared for serialization as shown in GraphEdX. Writing
+ * the graph to a file using the XMLEncoder is also demonstrated there.
+ * 
+ * @version 1.0 06-DEC-05
+ * @author Gaudenz Alder
+ */
+public class SerialGraph {
+
+	public static String FILENAME = "test.tmp";
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JGraph graph = new JGraph();
+		writeObject(graph, FILENAME);
+		graph = (JGraph) readObject(FILENAME);
+		JFrame frame = new JFrame("SerialGraph");
+		frame.getContentPane().add(graph);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void writeObject(Object object, String filename) {
+		try {
+			ObjectOutputStream out = new ObjectOutputStream(
+					new BufferedOutputStream(new FileOutputStream(filename)));
+			out.writeObject(object);
+			out.flush();
+			out.close();
+			File file = new File(filename);
+			System.out.println("File size is " + file.length() + " byte(s)");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Object readObject(String filename) {
+		try {
+			ObjectInputStream in = new ObjectInputStream(
+					new BufferedInputStream(new FileInputStream(filename)));
+			Object object = in.readObject();
+			in.close();
+			File file = new File(filename);
+			file.delete();
+			return object;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}

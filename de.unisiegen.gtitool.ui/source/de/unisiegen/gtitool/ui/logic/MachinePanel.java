422c7eee770389004b24680ce260a706b5b995fa
Benjamin Mies <benjamin.mies@googlemail.com>
2007-11-02 18:09:05 +0000

    - redesigned the machine view 	- added a toolbar 	- added mouse listener for the toolbar buttons 	- added a jgraph panel to create a diagram
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/gtitool/trunk@82 d1fb0f2a-2248-4f96-a540-7c12a6a72a62

--- de.unisiegen.gtitool.ui/source/de/unisiegen/gtitool/ui/logic/MachinePanel.java
+++ de.unisiegen.gtitool.ui/source/de/unisiegen/gtitool/ui/logic/MachinePanel.java
@@ -1,27 +1,555 @@
 package de.unisiegen.gtitool.ui.logic;
 
 
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.Rectangle2D;
+
+import javax.swing.BorderFactory;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import org.jgraph.JGraph;
+import org.jgraph.graph.DefaultEdge;
+import org.jgraph.graph.DefaultGraphCell;
+import org.jgraph.graph.DefaultGraphModel;
+import org.jgraph.graph.GraphConstants;
+import org.jgraph.graph.GraphModel;
+
 import de.unisiegen.gtitool.ui.EditorPanel;
+import de.unisiegen.gtitool.ui.jgraphcomponents.GPCellViewFactory;
 import de.unisiegen.gtitool.ui.netbeans.MachinesPanelForm;
 
 
 public class MachinePanel implements EditorPanel
 {
 
+  JFrame parent;
+
+
+  JPanel alphabetButtons;
+
+
   MachinesPanelForm machinePanel;
 
 
-  public MachinePanel (JPanel alphabetPanel)
+  JGraph graph;
+
+
+  MouseListener mouseListener;
+
+
+  MouseListener mouse;
+
+
+  MouseListener addState;
+
+
+  MouseListener transition;
+
+
+  MouseListener removeState;
+
+
+  MouseListener start;
+
+
+  MouseListener end;
+
+
+  public MachinePanel ( JFrame pParent, JPanel alphabetPanel )
   {
+    this.parent = pParent;
+    this.alphabetButtons = alphabetPanel;
+    initializeGraph ();
     this.machinePanel = new MachinesPanelForm ();
-    this.machinePanel.alphabetPanel.add ( alphabetPanel );
+    this.machinePanel.setMachinePanel ( this );
+    //this.machinePanel.alphabetPanel.add ( alphabetPanel );
+
+    this.machinePanel.diagrammContentPanel.setViewportView ( this.graph );
+
+    intitializeMouseListener ();
+
+    this.mouseListener = this.mouse;
+    this.graph.addMouseListener ( this.mouseListener );
+    this.graph.setSizeable ( false );
+
   }
 
 
   public JPanel getPanel ()
   {
     return this.machinePanel;
+  }
+
+
+  private void initializeGraph ()
+  {
+    // Construct Model and Graph
+    GraphModel model = new DefaultGraphModel ();
+    this.graph = new JGraph ( model );
+
+    graph.getGraphLayoutCache ().setFactory ( new GPCellViewFactory () );
+
+    // Control-drag should clone selection
+    graph.setCloneable ( true );
+
+    // Enable edit without final RETURN keystroke
+    graph.setInvokesStopCellEditing ( true );
+
+    // When over a cell, jump to its default port (we only have one, anyway)
+    graph.setJumpToDefaultPort ( true );
+
+    // Insert all three cells in one call, so we need an array to store them
+    DefaultGraphCell [] cells = new DefaultGraphCell [ 7 ];
+
+    cells [ 0 ] = createVertex ( 20, 20, "hello", Color.BLUE );
+
+    cells [ 1 ] = createVertex ( 140, 25, "brave", null );
+
+    cells [ 2 ] = createVertex ( 20, 145, "new", null );
+
+    cells [ 3 ] = createVertex ( 140, 140, "world", Color.ORANGE );
+
+    // Create Edges
+    DefaultEdge edge0 = new DefaultEdge ( "Test" );
+    edge0.setSource ( cells [ 0 ].getChildAt ( 0 ) );
+    edge0.setTarget ( cells [ 1 ].getChildAt ( 0 ) );
+    cells [ 4 ] = edge0;
+    GraphConstants.setLineEnd ( edge0.getAttributes (),
+        GraphConstants.ARROW_CIRCLE );
+    GraphConstants.setEndFill ( edge0.getAttributes (), true );
+
+    DefaultEdge edge1 = new DefaultEdge ();
+    edge1.setSource ( cells [ 1 ].getChildAt ( 0 ) );
+    edge1.setTarget ( cells [ 2 ].getChildAt ( 0 ) );
+    cells [ 5 ] = edge1;
+    GraphConstants.setLineEnd ( edge1.getAttributes (),
+        GraphConstants.ARROW_CLASSIC );
+    GraphConstants.setEndFill ( edge1.getAttributes (), true );
+
+    DefaultEdge edge2 = new DefaultEdge ();
+    edge2.setSource ( cells [ 2 ].getChildAt ( 0 ) );
+    edge2.setTarget ( cells [ 3 ].getChildAt ( 0 ) );
+    cells [ 6 ] = edge2;
+    GraphConstants.setLineEnd ( edge2.getAttributes (),
+        GraphConstants.ARROW_DIAMOND );
+    GraphConstants.setEndFill ( edge2.getAttributes (), true );
+
+    // Insert the cells via the cache, so they get selected
+    graph.getGraphLayoutCache ().insert ( cells );
+  }
+
+
+  public static DefaultGraphCell createVertex ( double x, double y,
+      String name, Color bg )
+  {
+
+    String viewClass = "de.unisiegen.gtitool.ui.jgraphcomponents.JGraphEllipseView";
+    double w = 40;
+    double h = 40;
+    boolean raised = false;
+    DefaultGraphCell cell = new DefaultGraphCell ( name );
+
+    // set the view class (indirection for the renderer and the editor)
+    if ( viewClass != null )
+      GPCellViewFactory.setViewClass ( cell.getAttributes (), viewClass );
+
+    // Set bounds
+    GraphConstants.setBounds ( cell.getAttributes (), new Rectangle2D.Double (
+        x, y, w, h ) );
+
+    // Set fill color
+    if ( bg != null )
+    {
+      GraphConstants.setGradientColor ( cell.getAttributes (), bg );
+      GraphConstants.setOpaque ( cell.getAttributes (), true );
+    }
+
+    // Set raised border
+    if ( raised )
+      GraphConstants.setBorder ( cell.getAttributes (), BorderFactory
+          .createRaisedBevelBorder () );
+    else
+      // Set black border
+      GraphConstants.setBorderColor ( cell.getAttributes (), Color.black );
+
+    // Add a Floating Port
+    cell.addPort ();
+
+    return cell;
+  }
+
+
+  @SuppressWarnings ( "unused" )
+  private void intitializeMouseListener ()
+  {
+    this.mouse = new MouseListener ()
+    {
+
+      /**
+       * Invoked when the mouse button has been clicked (pressed and released)
+       * on a component.
+       */
+      public void mouseClicked ( MouseEvent e )
+      {
+        System.err.println ( "Mouse" );
+      }
+
+
+      /**
+       * Invoked when a mouse button has been pressed on a component.
+       */
+      public void mousePressed ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+
+
+      /**
+       * Invoked when a mouse button has been released on a component.
+       */
+      public void mouseReleased ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+
+
+      /**
+       * Invoked when the mouse enters a component.
+       */
+      public void mouseEntered ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+
+
+      /**
+       * Invoked when the mouse exits a component.
+       */
+      public void mouseExited ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+    };
+
+    this.addState = new MouseListener ()
+    {
+
+      /**
+       * Invoked when the mouse button has been clicked (pressed and released)
+       * on a component.
+       */
+      public void mouseClicked ( MouseEvent e )
+      {
+        MachinePanel.this.graph.getGraphLayoutCache ().insert (
+            createVertex ( e.getPoint ().x, e.getPoint ().y, "Neuer Zustand",
+                null ) );
+      }
+
+
+      /**
+       * Invoked when a mouse button has been pressed on a component.
+       */
+      public void mousePressed ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+
+
+      /**
+       * Invoked when a mouse button has been released on a component.
+       */
+      public void mouseReleased ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+
+
+      /**
+       * Invoked when the mouse enters a component.
+       */
+      public void mouseEntered ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+
+
+      /**
+       * Invoked when the mouse exits a component.
+       */
+      public void mouseExited ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+    };
+
+    this.transition = new MouseListener ()
+    {
+
+      /**
+       * Invoked when the mouse button has been clicked (pressed and released)
+       * on a component.
+       */
+      public void mouseClicked ( MouseEvent e )
+      {
+        System.err.println ( "Transition" );
+      }
+
+
+      /**
+       * Invoked when a mouse button has been pressed on a component.
+       */
+      public void mousePressed ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+
+
+      /**
+       * Invoked when a mouse button has been released on a component.
+       */
+      public void mouseReleased ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+
+
+      /**
+       * Invoked when the mouse enters a component.
+       */
+      public void mouseEntered ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+
+
+      /**
+       * Invoked when the mouse exits a component.
+       */
+      public void mouseExited ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+    };
+
+    this.removeState = new MouseListener ()
+    {
+
+      /**
+       * Invoked when the mouse button has been clicked (pressed and released)
+       * on a component.
+       */
+      public void mouseClicked ( MouseEvent e )
+      {
+        System.err.println ( "Remove State" );
+      }
+
+
+      /**
+       * Invoked when a mouse button has been pressed on a component.
+       */
+      public void mousePressed ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+
+
+      /**
+       * Invoked when a mouse button has been released on a component.
+       */
+      public void mouseReleased ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+
+
+      /**
+       * Invoked when the mouse enters a component.
+       */
+      public void mouseEntered ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+
+
+      /**
+       * Invoked when the mouse exits a component.
+       */
+      public void mouseExited ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+    };
+
+    this.start = new MouseListener ()
+    {
+
+      /**
+       * Invoked when the mouse button has been clicked (pressed and released)
+       * on a component.
+       */
+      public void mouseClicked ( MouseEvent e )
+      {
+        System.err.println ( "Start" );
+      }
+
+
+      /**
+       * Invoked when a mouse button has been pressed on a component.
+       */
+      public void mousePressed ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+
+
+      /**
+       * Invoked when a mouse button has been released on a component.
+       */
+      public void mouseReleased ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+
+
+      /**
+       * Invoked when the mouse enters a component.
+       */
+      public void mouseEntered ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+
+
+      /**
+       * Invoked when the mouse exits a component.
+       */
+      public void mouseExited ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+    };
+
+    this.end = new MouseListener ()
+    {
+
+      /**
+       * Invoked when the mouse button has been clicked (pressed and released)
+       * on a component.
+       */
+      public void mouseClicked ( MouseEvent e )
+      {
+        System.err.println ( "End" );
+      }
+
+
+      /**
+       * Invoked when a mouse button has been pressed on a component.
+       */
+      public void mousePressed ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+
+
+      /**
+       * Invoked when a mouse button has been released on a component.
+       */
+      public void mouseReleased ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+
+
+      /**
+       * Invoked when the mouse enters a component.
+       */
+      public void mouseEntered ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+
+
+      /**
+       * Invoked when the mouse exits a component.
+       */
+      public void mouseExited ( MouseEvent e )
+      {
+        // Nothing to do here
+      }
+    };
+  }
+
+
+  public void handleToolbarAddState ( boolean state )
+  {
+    if ( state )
+      this.graph.addMouseListener ( this.addState );
+    else
+      this.graph.removeMouseListener ( this.addState );
+  }
+
+
+  public void handleToolbarMouse ( boolean state )
+  {
+    if ( state )
+      this.graph.addMouseListener ( this.mouse );
+    else
+      this.graph.removeMouseListener ( this.mouse );
+  }
+
+
+  public void handleToolbarTransition ( boolean state )
+  {
+    if ( state )
+      this.graph.addMouseListener ( this.transition );
+    else
+      this.graph.removeMouseListener ( this.transition );
+  }
+
+
+  public void handleToolbarDelState ( boolean state )
+  {
+    if ( state )
+      this.graph.addMouseListener ( this.removeState );
+    else
+      this.graph.removeMouseListener ( this.removeState );
+  }
+
+
+  public void handleToolbarStart ( boolean state )
+  {
+    if ( state )
+      this.graph.addMouseListener ( this.start );
+    else
+      this.graph.removeMouseListener ( this.start );
+  }
+
+
+  public void handleToolbarEnd ( boolean state )
+  {
+    if ( state )
+      this.graph.addMouseListener ( this.end );
+    else
+      this.graph.removeMouseListener ( this.end );
+  }
+
+
+  public void handleToolbarAlphabet ()
+  {
+    JDialog alphabet = new JDialog ( this.parent, true );
+    alphabet.setContentPane ( this.alphabetButtons );
+    alphabet.setTitle ( "Edit Alphabet" );
+    alphabet.pack();
+    int x = this.parent.getBounds ().x + ( this.parent.getWidth () / 2 )
+        - ( alphabet.getWidth () / 2 );
+    int y = this.parent.getBounds ().y + ( this.parent.getHeight () / 2 )
+        - ( alphabet.getHeight () / 2 );
+    alphabet.setBounds ( x, y, alphabet.getWidth (), alphabet.getHeight () );
+    alphabet.setVisible ( true );
   }
 }
--- de.unisiegen.gtitool.ui/source/de/unisiegen/gtitool/ui/netbeans/MachinesPanelForm.java
+++ de.unisiegen.gtitool.ui/source/de/unisiegen/gtitool/ui/netbeans/MachinesPanelForm.java
@@ -6,6 +6,8 @@
 
 package de.unisiegen.gtitool.ui.netbeans;
 
+import de.unisiegen.gtitool.ui.logic.MachinePanel;
+
 /**
  *
  * @author  Benjamin Mies
@@ -26,61 +28,215 @@ public class MachinesPanelForm extends javax.swing.JPanel {
     private void initComponents() {
         java.awt.GridBagConstraints gridBagConstraints;
 
-        tabbedPane = new javax.swing.JTabbedPane();
-        diagrammPanel = new javax.swing.JPanel();
-        talbePanel = new javax.swing.JPanel();
-        alphabetPanel = new javax.swing.JPanel();
+        toolbarButton = new javax.swing.ButtonGroup();
+        toolbar = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        mouse = new javax.swing.JToggleButton();
+        addState = new javax.swing.JToggleButton();
+        transition = new javax.swing.JToggleButton();
+        deleteState = new javax.swing.JToggleButton();
+        start = new javax.swing.JToggleButton();
+        end = new javax.swing.JToggleButton();
+        jButton1 = new javax.swing.JButton();
+        jSplitPane1 = new javax.swing.JSplitPane();
+        diagramm = new javax.swing.JPanel();
+        diagrammContentPanel = new javax.swing.JScrollPane();
+        tablePanel = new javax.swing.JPanel();
 
         setLayout(new java.awt.GridBagLayout());
 
-        tabbedPane.setName("machines");
-        tabbedPane.addChangeListener(new javax.swing.event.ChangeListener() {
-            public void stateChanged(javax.swing.event.ChangeEvent evt) {
-                tabbedPaneStateChanged(evt);
+        toolbar.setLayout(new java.awt.GridBagLayout());
+
+        jLabel1.setText("Toolbar");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        toolbar.add(jLabel1, gridBagConstraints);
+
+        toolbarButton.add(mouse);
+        mouse.setSelected(true);
+        mouse.setText("mouse");
+        mouse.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                handleToolbarMouse(evt);
             }
         });
-        tabbedPane.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyPressed(java.awt.event.KeyEvent evt) {
-                tabbedPaneKeyPressed(evt);
-            }
-        });
-
-        diagrammPanel.setLayout(new java.awt.GridBagLayout());
-
-        tabbedPane.addTab("Diagramm", diagrammPanel);
-
-        talbePanel.setLayout(new java.awt.GridBagLayout());
-
-        tabbedPane.addTab("Tabelle", talbePanel);
-
-        alphabetPanel.setLayout(new java.awt.GridBagLayout());
-
-        tabbedPane.addTab("Alphabet", alphabetPanel);
 
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 1.0;
+        toolbar.add(mouse, gridBagConstraints);
+
+        toolbarButton.add(addState);
+        addState.setText("addState");
+        addState.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                handleToolbarAddState(evt);
+            }
+        });
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 1.0;
+        toolbar.add(addState, gridBagConstraints);
+
+        toolbarButton.add(transition);
+        transition.setText("transition");
+        transition.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                handleToolBarTransition(evt);
+            }
+        });
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 1.0;
+        toolbar.add(transition, gridBagConstraints);
+
+        toolbarButton.add(deleteState);
+        deleteState.setText("delState");
+        deleteState.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                handleToolbarDelState(evt);
+            }
+        });
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 1.0;
+        toolbar.add(deleteState, gridBagConstraints);
+
+        toolbarButton.add(start);
+        start.setText("start");
+        start.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                handleToolbarStart(evt);
+            }
+        });
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridy = 5;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 1.0;
+        toolbar.add(start, gridBagConstraints);
+
+        toolbarButton.add(end);
+        end.setText("end");
+        end.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                handleToolbarEnd(evt);
+            }
+        });
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridy = 6;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 1.0;
+        toolbar.add(end, gridBagConstraints);
+
+        jButton1.setText("alphabet");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                handleToolbarAlphabet(evt);
+            }
+        });
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridy = 7;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        toolbar.add(jButton1, gridBagConstraints);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTH;
+        add(toolbar, gridBagConstraints);
+
+        jSplitPane1.setOrientation(javax.swing.JSplitPane.VERTICAL_SPLIT);
+        diagramm.setLayout(new java.awt.GridBagLayout());
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
         gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-        gridBagConstraints.weightx = 2.0;
-        gridBagConstraints.weighty = 2.0;
-        add(tabbedPane, gridBagConstraints);
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        diagramm.add(diagrammContentPanel, gridBagConstraints);
+
+        jSplitPane1.setLeftComponent(diagramm);
+
+        jSplitPane1.setRightComponent(tablePanel);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        add(jSplitPane1, gridBagConstraints);
 
     }// </editor-fold>//GEN-END:initComponents
 
-    private void tabbedPaneKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_tabbedPaneKeyPressed
-// NOTICE Ihre Ereignisbehandlung hier einfügen:
-    }//GEN-LAST:event_tabbedPaneKeyPressed
+    private void handleToolbarMouse(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_handleToolbarMouse
+        this.logic.handleToolbarMouse(this.mouse.isSelected());
+    }//GEN-LAST:event_handleToolbarMouse
 
-    private void tabbedPaneStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_tabbedPaneStateChanged
-// NOTICE Ihre Ereignisbehandlung hier einfügen:
-    }//GEN-LAST:event_tabbedPaneStateChanged
+    private void handleToolbarAddState(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_handleToolbarAddState
+        this.logic.handleToolbarAddState(this.addState.isSelected());
+    }//GEN-LAST:event_handleToolbarAddState
+
+    private void handleToolBarTransition(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_handleToolBarTransition
+        this.logic.handleToolbarTransition(this.transition.isSelected());
+    }//GEN-LAST:event_handleToolBarTransition
+
+    private void handleToolbarDelState(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_handleToolbarDelState
+        this.logic.handleToolbarDelState(this.deleteState.isSelected());
+    }//GEN-LAST:event_handleToolbarDelState
+
+    private void handleToolbarStart(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_handleToolbarStart
+        this.logic.handleToolbarStart(this.start.isSelected());
+    }//GEN-LAST:event_handleToolbarStart
+
+    private void handleToolbarEnd(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_handleToolbarEnd
+        this.logic.handleToolbarEnd(this.end.isSelected());
+    }//GEN-LAST:event_handleToolbarEnd
+
+    private void handleToolbarAlphabet(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_handleToolbarAlphabet
+        this.logic.handleToolbarAlphabet();
+    }//GEN-LAST:event_handleToolbarAlphabet
     
     
     // Variablendeklaration - nicht modifizieren//GEN-BEGIN:variables
-    public javax.swing.JPanel alphabetPanel;
-    private javax.swing.JPanel diagrammPanel;
-    private javax.swing.JTabbedPane tabbedPane;
-    private javax.swing.JPanel talbePanel;
+    private javax.swing.JToggleButton addState;
+    private javax.swing.JToggleButton deleteState;
+    private javax.swing.JPanel diagramm;
+    public javax.swing.JScrollPane diagrammContentPanel;
+    private javax.swing.JToggleButton end;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JSplitPane jSplitPane1;
+    private javax.swing.JToggleButton mouse;
+    private javax.swing.JToggleButton start;
+    private javax.swing.JPanel tablePanel;
+    private javax.swing.JPanel toolbar;
+    private javax.swing.ButtonGroup toolbarButton;
+    private javax.swing.JToggleButton transition;
     // Ende der Variablendeklaration//GEN-END:variables
     
+    
+        private MachinePanel logic;
+    
+    public void setMachinePanel (MachinePanel window){
+        logic = window;
+    }
 }
+
+

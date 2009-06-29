4559869d3696bda5a5fba0a8fa09a5541dc1a906
Christian Fehler <christian.fehler@gmx.de>
2009-06-29 21:50:54 +0000

    - Update the copyright year.
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/gtitool/trunk@1590 d1fb0f2a-2248-4f96-a540-7c12a6a72a62

--- start/buildfiles/executer/linux/gtitool.sh
+++ start/buildfiles/executer/linux/gtitool.sh
@@ -2,7 +2,7 @@
 #
 # $Id:gtitool.sh 57 2007-10-22 23:43:33Z fehler $
 #
-# Copyright (c) 2007-2008 Christian Fehler
+# Copyright (c) 2007-2009 Christian Fehler
 #
 
 exec java -jar "gtitool.start-@gtitool.version@.jar" "$@"

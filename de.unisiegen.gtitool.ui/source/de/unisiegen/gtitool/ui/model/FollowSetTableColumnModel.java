355cad4d820cd817099dff5597aae802b083d4af
Christian Uhrhan <chris.uhrhan@googlemail.com>
2010-04-06 12:50:19 +0000

    * Added FollowSetTable(Column)Model to display <nonterminal,followset> data
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/gtitool/trunk@1790 d1fb0f2a-2248-4f96-a540-7c12a6a72a62

---
+++ de.unisiegen.gtitool.ui/source/de/unisiegen/gtitool/ui/model/FollowSetTableColumnModel.java
@@ -0,0 +1,59 @@
+package de.unisiegen.gtitool.ui.model;
+
+
+import javax.swing.table.DefaultTableColumnModel;
+import javax.swing.table.TableColumn;
+
+import de.unisiegen.gtitool.core.entities.NonterminalSymbol;
+import de.unisiegen.gtitool.core.parser.style.PrettyString;
+import de.unisiegen.gtitool.core.parser.style.PrettyToken;
+import de.unisiegen.gtitool.core.parser.style.Style;
+import de.unisiegen.gtitool.core.parser.style.renderer.PrettyStringTableHeaderCellRenderer;
+import de.unisiegen.gtitool.ui.i18n.Messages;
+
+
+/**
+ * Defines the columns used to display <Nonterminal,FollowSet> data
+ */
+public class FollowSetTableColumnModel extends DefaultTableColumnModel
+{
+
+  /**
+   * The generated serial
+   */
+  private static final long serialVersionUID = 2841053177088812823L;
+
+
+  /**
+   * The {@link NonterminalSymbol} column index
+   */
+  private static final int NONTERMINAL_COLUMN = 0;
+
+
+  /**
+   * The follow set column index
+   */
+  private static final int FOLLOW_SET_COLUMN = 1;
+
+
+  /**
+   * Allocates a new {@link FollowSetTableColumnModel}
+   */
+  public FollowSetTableColumnModel ()
+  {
+    TableColumn symbolColumn = new TableColumn ( NONTERMINAL_COLUMN );
+    symbolColumn
+        .setHeaderValue ( new PrettyString (
+            new PrettyToken (
+                Messages
+                    .getString ( "FollowSetColumnModel.NonterminalSymbolColumn" ), Style.NONE ) ) ); //$NON-NLS-1$
+    symbolColumn
+        .setHeaderRenderer ( new PrettyStringTableHeaderCellRenderer () );
+
+    symbolColumn = new TableColumn ( FOLLOW_SET_COLUMN );
+    symbolColumn.setHeaderValue ( new PrettyString ( new PrettyToken ( Messages
+        .getString ( "FollowSetColumnModel.FollowSetColumn" ), Style.NONE ) ) ); //$NON-NLS-1$
+    symbolColumn
+        .setHeaderRenderer ( new PrettyStringTableHeaderCellRenderer () );
+  }
+}
---
+++ de.unisiegen.gtitool.ui/source/de/unisiegen/gtitool/ui/model/FollowSetTableModel.java
@@ -0,0 +1,119 @@
+package de.unisiegen.gtitool.ui.model;
+
+
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+import de.unisiegen.gtitool.core.entities.NonterminalSymbol;
+import de.unisiegen.gtitool.core.entities.NonterminalSymbolSet;
+import de.unisiegen.gtitool.core.entities.TerminalSymbolSet;
+import de.unisiegen.gtitool.core.exceptions.grammar.GrammarInvalidNonterminalException;
+import de.unisiegen.gtitool.core.exceptions.terminalsymbolset.TerminalSymbolSetException;
+import de.unisiegen.gtitool.core.grammars.cfg.CFG;
+import de.unisiegen.gtitool.core.parser.style.PrettyString;
+
+
+/**
+ * Table model to present <Nonterminal,FollowSet> data
+ * 
+ * @author Christian Uhrhan
+ */
+public class FollowSetTableModel extends AbstractTableModel
+{
+
+  /**
+   * The generated serial
+   */
+  private static final long serialVersionUID = 2021815057054014805L;
+
+
+  /**
+   * The {@link NonterminalSymbol} column index
+   */
+  private static final int NONTERMINAL_COLUMN = 0;
+
+
+  /**
+   * The follow set column index
+   */
+  private static final int FOLLOW_SET_COLUMN = 1;
+
+
+  /**
+   * Number of columns
+   */
+  private static final int COLUMN_COUNT = 2;
+
+
+  /**
+   * The {@link NonterminalSymbolSet}
+   */
+  private NonterminalSymbolSet nonterminals;
+
+
+  /**
+   * The list of follow sets
+   */
+  private ArrayList < TerminalSymbolSet > followSets;
+
+
+  /**
+   * Allocates a new {@link FollowSetTableModel}
+   * 
+   * @param cfg The {@link CFG}
+   * @throws TerminalSymbolSetException
+   * @throws GrammarInvalidNonterminalException
+   */
+  public FollowSetTableModel ( final CFG cfg )
+      throws GrammarInvalidNonterminalException, TerminalSymbolSetException
+  {
+    if ( cfg == null )
+      throw new NullPointerException ( "cfg is null" ); //$NON-NLS-1$
+
+    this.nonterminals = cfg.getNonterminalSymbolSet ();
+    for ( NonterminalSymbol ns : this.nonterminals )
+      this.followSets.add ( cfg.follow ( ns ) );
+  }
+
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see javax.swing.table.TableModel#getColumnCount()
+   */
+  public int getColumnCount ()
+  {
+    return COLUMN_COUNT;
+  }
+
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see javax.swing.table.TableModel#getRowCount()
+   */
+  public int getRowCount ()
+  {
+    return this.nonterminals.size ();
+  }
+
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see javax.swing.table.TableModel#getValueAt(int, int)
+   */
+  public Object getValueAt ( int rowIndex, int columnIndex )
+  {
+    switch ( columnIndex )
+    {
+      case NONTERMINAL_COLUMN :
+        return this.nonterminals.get ( rowIndex );
+      case FOLLOW_SET_COLUMN :
+        return this.followSets.get ( rowIndex );
+    }
+    return new PrettyString ();
+  }
+
+}

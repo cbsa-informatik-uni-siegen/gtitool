2261248114b7931f1bba8c682322c5f30d2a9300
Philipp Reh <sefi@s-e-f-i.de>
2010-05-04 11:09:20 +0000

    Removed obsolete debug output.
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/gtitool/trunk@1877 d1fb0f2a-2248-4f96-a540-7c12a6a72a62

--- de.unisiegen.gtitool.core/source-impl/de/unisiegen/gtitool/core/machines/lr/SLRParser.java
+++ de.unisiegen.gtitool.core/source-impl/de/unisiegen/gtitool/core/machines/lr/SLRParser.java
@@ -36,10 +36,6 @@ public class SLRParser extends DefaultLR0Parser
   protected boolean followCondition ( final LR0Item item,
       final TerminalSymbol symbol ) throws GrammarInvalidNonterminalException
   {
-    System.out.println ( "terminal: " + symbol + ", nonterminal: "
-        + item.getNonterminalSymbol () + ", : "
-        + getGrammar ().follow ( item.getNonterminalSymbol () ) );
-
     return getGrammar ().follow ( item.getNonterminalSymbol () ).contains (
         symbol );
   }
